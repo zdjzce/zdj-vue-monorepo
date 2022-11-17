a2d414167374b668b2ba803d4a1242bd26fe3ff4
githubzdj <493904954@qq.com>
2022-11-17 23:59:23 +0800

    feat: watch invalidate

--- packages/reactivity/__test__/watch.test.ts
+++ packages/reactivity/__test__/watch.test.ts
@@ -1,8 +1,11 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { watch } from '../src/watch'
 import { reactive } from '../src/index'
 
 describe('watch', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
   it('watch 触发更改执行', () => {
     const data = {
       foo: 1
@@ -17,11 +20,58 @@ describe('watch', () => {
       immediate: true
     })
 
-    expect(temp).toBe(undefined)
+    expect(temp).toBe(undefined)  // 立马执行watch old为undefined 符合预期
     expect(fn).toBeCalledTimes(1)
 
     obj.foo = 2
     expect(temp).toBe(1)
   })
 
+  it('watch flush', async () => {
+    const data = {
+      foo: 1
+    }
+    const obj = reactive(data)
+    const fn = vi.fn((newVal, oldVal) => { })
+    watch(() => obj.foo, (newVal: any, oldVal: any) => {
+      fn(newVal, oldVal)
+    }, {
+      immediate: true,
+      flush: 'post'
+    })
+
+    obj.foo += 1
+    expect(fn).toBeCalledTimes(1)
+    await vi.runAllTicks()
+    expect(fn).toBeCalledTimes(2)
+  })
+
+  it('watch invalidate', () => {
+    const data = {
+      foo: 1
+    }
+    let temp = 0
+    const obj = reactive(data)
+    const fn = vi.fn(() => { })
+    watch(() => obj.foo, async (newVal, oldVal, onInvalidate) => {
+      let expired = false
+      onInvalidate(() => {
+        // 期望下次执行set出发watch回调时 执行cleanup 将 expired 变为TRUe 让回调过期
+        expired = true
+      })
+
+      let res = 0
+      await setTimeout(() => {
+        res = 1
+      }, 500)
+      if (!expired) {
+        fn()
+        temp += res
+      }
+    })
+    obj.foo++
+    obj.foo++
+    expect(temp).toBe(1)
+    expect(fn).toBeCalledTimes(1)
+  })
 })
--- packages/reactivity/src/watch.ts
+++ packages/reactivity/src/watch.ts
@@ -5,7 +5,7 @@ interface ArgumentFun<T> {
   (): T
 }
 interface CallBack<T> {
-  (oldVal: any, newVal: any): T
+  (oldVal: any, newVal: any, onInvalidate: (fn: ArgumentFun<any>) => any): T
 }
 type Source = ArgumentFun<any> | any
 
@@ -13,20 +13,40 @@ type Source = ArgumentFun<any> | any
 export function watch(source: Source, cb: CallBack<void | any>, options?: any) {
   const getter = source instanceof Function ? source : traverse(source)
   let newVal, oldVal
+  const p = Promise.resolve()
+  let cleanUp
 
   const effectFn = effect(getter, {
-    scheduler: job,
+    scheduler: () => {
+      // 放到微任务队列中 等待 DOM 更新结束后执行
+      if (options && options.flush === 'post') {
+        p.then(job)
+      } else {
+        job()
+      }
+    },
     lazy: true
   })
 
   function job() {
     newVal = effectFn()
-    cb(newVal, oldVal)
+
+    // 每次执行回调之前 先检查是否存在过期回调
+    // 如果存在用户传递的过期回调 就执行 此时说明已经是新的状态 
+    if (cleanUp) {
+      cleanUp()
+    }
+
+    cb(newVal, oldVal, onInvalidate)
     // 需要把旧的值换成新的值
     oldVal = newVal
   }
 
-  if (options.immediate) {
+  function onInvalidate(fn) {
+    cleanUp = fn
+  }
+
+  if (options && options.immediate) {
     job()
   } else {
     oldVal = effectFn()
